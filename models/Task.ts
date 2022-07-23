a5ef930fb64f619f3db83889b37f27c735ce2f00
Francisco Wendeburg <wendeburgf@gmail.com>
2022-07-23 05:53:59 +0200

    adds the Task and User models

---
+++ models/Task.ts
@@ -0,0 +1,49 @@
+import { Document, Schema, model, Types } from 'mongoose';
+import { IUser } from './User';
+
+enum TaskPriority {
+    Low,
+    Medium,
+    High
+}
+
+interface ITask extends Document {
+    name: string,
+    description?: string,
+    dueDate?: Date,
+    dateAdded: Date,
+    priority?: TaskPriority
+    isCompleted: boolean,
+    owner: IUser["_id"]
+}
+
+const TaskSchema = new Schema({
+    name: {
+        type: String, 
+        lowercase: true, 
+        required: true, 
+        trim: true,
+        maxLength: 100},
+    description: { type: String },
+    dueDate: {
+        type: Date,
+        validate: [function(this: ITask) {
+            if (this.dueDate instanceof Date) {
+                return this.dueDate > this.dateAdded;
+            }
+            
+            return true;
+        }]
+    },
+    dateAdded: { type: Date, required: true },
+    priority: { type: Number, enum: [0, 1, 2], required: true },
+    isCompleted: { type: Boolean, required: true },
+    owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+export {
+    ITask,
+    TaskSchema
+}
+
+export default model<Itask>('Task', TaskSchema);
---
+++ models/User.ts
@@ -0,0 +1,35 @@
+import { Document, Schema, model } from 'mongoose';
+import { TaskSchema, ITask } from './Task';
+
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    profilePicture?: string;
+    tasks: ITask[];
+}
+
+const UserSchema = new Schema({
+    name: {
+        type: String, 
+        lowercase: true, 
+        required: true, 
+        trim: true,
+        maxLength: 25},
+    email: {type: String, 
+        lowercase: true, 
+        required: true, 
+        trim: true,
+        maxLength: 25,
+        match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/},
+    password: {type: String, required: true, maxLength: 25},
+    profilePicture: String,
+    tasks: {type: [{ type: Schema.Types.ObjectId, ref: 'Task' }], required: true}
+});
+
+export {
+    IUser,
+    UserSchema
+}
+
+export default model<IUser>('User', UserSchema);
