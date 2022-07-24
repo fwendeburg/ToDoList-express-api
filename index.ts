1886a91d7f603ebcc638eeb9dc3320a8f5c5200f
Francisco Wendeburg <wendeburgf@gmail.com>
2022-07-24 05:58:37 +0200

    adds authentication

--- index.ts
+++ index.ts
@@ -1,6 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { connect, ConnectOptions } from 'mongoose';
+import indexRouter from './routes/indexRoutes';
+import taskRouter from './routes/tasksRoutes';
+import projectsRouter from './routes/projectsRoutes';
+import userRouter from './routes/userRoutes';
+import authRouter from './routes/authRoutes';
+import passport from "passport";
+import "./passportConfig";
 
 dotenv.config();
 
@@ -25,9 +32,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use('/', authRouter);
+app.use('/', indexRouter);
+//TODO /app routes.
+app.use('/tasks', passport.authenticate('jwt', { session: false }), taskRouter);
+app.use('/projects', passport.authenticate('jwt', { session: false }), projectsRouter);
+app.use('/users', passport.authenticate('jwt', { session: false }), userRouter);
+
 
 app.use(function (req: Request, res: Response) {
   res.sendStatus(404);
