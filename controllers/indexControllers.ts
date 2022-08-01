0549d73323d0e97ac5905e43585abacdac50f5cb
Francisco Wendeburg <wendeburgf@gmail.com>
2022-08-01 23:25:04 +0200

    remove the '/' route from the API

--- controllers/indexControllers.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from "express";
-
-function index(req: Request, res: Response) {
-    res.send("NOT IMPLEMENTED");
-}
-
-export {
-    index
-}
--- routes/indexRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { index } from "../controllers/indexControllers";
-
-let indexRouter = Router();
-
-indexRouter.get('/', index);
-
-export default indexRouter;
