c8065e3a245f19315b7d2282efc7e6e17c03ecba
Francisco Wendeburg <wendeburgf@gmail.com>
2022-09-10 23:19:21 +0200

    fixed a bug and changed the validation error message

--- models/User.ts
+++ models/User.ts
@@ -24,11 +24,11 @@ const UserSchema = new Schema({
         unique: true,
         validate: {
             validator: async function(this: IUser): Promise<boolean> {
-                        const user = await UserModel.find({email: this.email});
+                        const user = await UserModel.findOne({email: this.email});
 
-                        return !user;
+                        return (user == null || user._id.toString() == this._id.toString());
                     },
-            message: "There already exists a user with this email"
+            message: "A user with this email already exists"
         }
     },
     password: { 
