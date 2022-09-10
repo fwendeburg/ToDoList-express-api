7f771de654534b149ec09b336623e1aa5099e9a5
Francisco Wendeburg <wendeburgf@gmail.com>
2022-09-10 03:26:46 +0200

    finished implementing user controllers

--- controllers/userControllers.ts
+++ controllers/userControllers.ts
@@ -1,36 +1,40 @@
-
 import { NextFunction, Request, Response } from "express";
 import { generatePassword, issueJWT, validatePassword } from "../authentication/utils";
 import UserModel from "../models/User";
 import { AuthenticatedRequest } from '../@types/ExpressExtended';
 
-function userLogin(req: Request, res: Response, next: NextFunction) {
-    UserModel.findOne({ email: req.body.email }).then(user => {
+async function userLogin(req: Request, res: Response, next: NextFunction) {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+
         if (!user) {
-            res.status(401).json({ success: false, msg: "user not found" });
+            res.status(200).json({ success: false, msg: "Could not find user with that email" });
         }
         else {
             const isPassValid = validatePassword(req.body.password, user.password, user.salt);
         
             if (isPassValid) {
                 const jwtToken = issueJWT(user);
-
+    
                 const userToSend = {
                     name: user.name,
                     email: user.email,
                     _id: user._id
                 }
-
-                res.status(200).json({ success: true, user: userToSend, token: jwtToken.token, expiresIn: jwtToken.expiresIn })
+    
+                res.status(200).json({ success: true, user: userToSend, token: jwtToken.token, expiresIn: jwtToken.expiresIn });
             }
             else {
-                res.status(401).json({ success: false, msg: "incorrect password" });
+                res.status(500).json({ success: false, msg: "Incorrect password" });
             }
         }
-    }).catch(error => next(error));
+    }
+    catch (err) {
+        next(err);
+    }
 }
 
-function registerUser(req: Request, res: Response, next: NextFunction) {
+async function registerUser(req: Request, res: Response, next: NextFunction) {
     const hashedPass = generatePassword(req.body.password);
 
     const newUser =  new UserModel({
@@ -41,35 +45,120 @@ function registerUser(req: Request, res: Response, next: NextFunction) {
         profilePicture: req.body.profilePicture
     });
 
-    newUser.save().then(user => {
-        const jwtToken = issueJWT(user);
+    try {
+        await newUser.save();
+    
+        const jwtToken = issueJWT(newUser);
 
         const userToSend = {
-            name: user.name,
-            email: user.email,
-            _id: user._id
+            name: newUser.name,
+            email: newUser.email,
+            _id: newUser._id
         }
 
         res.json({success: true, user: userToSend, token: jwtToken.token, expiresIn: jwtToken.expiresIn});
-    }).catch(error => next(error));
+    }
+    catch (err) {
+        next(err);
+    }
 }
 
-function userDetail(req: AuthenticatedRequest, res: Response) {
-    res.send("NOT IMPLEMENTED");
+async function userDetail(req: Request, res: Response) {
+    try {
+        const user = await UserModel.findById({_id: req.body.userid});
+        
+        if (user) {
+            res.status(200).json({success: true, user: {
+                name: user.name,
+                email: user.email
+            }});
+        }
+        else {
+            res.status(200).json({success: false, msg: "Could not find a user with that id"});
+        }
+    }
+    catch (err) {
+        res.status(500).json({success: false, msg: `Error while querying user: ${err}`})
+    }
 }
 
-function userDelete(req: AuthenticatedRequest, res: Response) {
-    res.send("NOT IMPLEMENTED");
+async function userDelete(request: Request, res: Response) {
+    const req = request as AuthenticatedRequest;
+    
+    try {
+        await UserModel.findByIdAndRemove({_id: req.user._id});
+    
+        res.status(200).json({success: true});
+    }
+    catch (err) {
+        res.status(500).json({success: false, msg: `Error while deleting user: ${err}`});
+    }
 }
 
-function userUpdate(req: AuthenticatedRequest, res: Response) {
-    res.send("NOT IMPLEMENTED");
+async function userUpdate(request: Request, res: Response) {
+    const req = request as AuthenticatedRequest;
+    
+    try {
+        let user = await UserModel.findById({_id: req.user._id});
+
+        if (user) {
+            user.name = req.body.name || user.name;
+            user.email = req.body.email || user.email;
+
+            await user.save();
+
+            res.status(200).json({success: true, user: {
+                name: user.name,
+                email: user.email
+            }});
+        }
+        else {
+            res.status(200).json({success: false, msg: "Could not find a user with that id"});
+        }
+    }
+    catch (err) {
+        res.status(500).json({success: false, msg: `Error while updating user: ${err}`});
+    }
+}
+
+async function userUpdatePassword(request: Request, res: Response) {
+    const req = request as AuthenticatedRequest;
+
+    try {
+        let user = await UserModel.findById({_id: req.user._id});
+
+        if (user) {
+            const hashedPass = generatePassword(req.body.password);
+
+            user.password = hashedPass.hash;
+            user.salt = hashedPass.salt;
+
+            await user.save();
+
+            const jwtToken = issueJWT(user);
+
+            const userToSend = {
+                name: user.name,
+                email: user.email,
+                _id: user._id
+            }
+
+            res.status(200).json({ success: true, user: userToSend, token: jwtToken.token, expiresIn: jwtToken.expiresIn });
+        }
+        else {
+            res.status(200).json({success: false, msg: "Could not find a user with that id"});
+        }
+    }
+    catch (err) {
+        res.status(500).json({success: false, msg: `Error while updating user password: ${err}`});
+    }
 }
 
 export {
     userDetail,
     userDelete,
     userUpdate,
+    userUpdatePassword,
     userLogin,
     registerUser
 }
